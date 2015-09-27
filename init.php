ff715c44cc371d0fd84c69b23f19a810b79bd3e0
isisadmin <isislab.unisa@gmail.com>
2015-09-27 16:51:00 +0200

    fix init

--- init.php
+++ init.php
@@ -1,13 +1,13 @@
 <?php
 
-/*define("ODE_DEEP_URL", 'http://service.routetopa.eu/DEEalerProvider/DEEP/');
+define("ODE_DEEP_URL", 'http://service.routetopa.eu/DEEalerProvider/DEEP/');
 define("ODE_DEEP_DATALET_LIST", 'http://service.routetopa.eu/DEEalerProvider/DEEP/datalets-list');
 define("ODE_DEEP_CLIENT", 'http://service.routetopa.eu/DEEalerProvider/DEEPCLIENT/js/deepClient.js');
-define("ODE_ORGANIZATION", 2);*/
+define("ODE_ORGANIZATION", 2);
 
-define("ODE_DEEP_URL", 'http://192.168.232.128/DEEalerProvider/DEEP/');
+/*define("ODE_DEEP_URL", 'http://192.168.232.128/DEEalerProvider/DEEP/');
 define("ODE_DEEP_DATALET_LIST", 'http://192.168.232.128/DEEalerProvider/DEEP/datalets-list');
 define("ODE_DEEP_CLIENT", 'http://192.168.232.128/DEEalerProvider/DEEPCLIENT/js/deepClient.js');
-define("ODE_ORGANIZATION", 2);
+define("ODE_ORGANIZATION", 2);*/
 
 ODE_CLASS_EventHandler::getInstance()->init();
