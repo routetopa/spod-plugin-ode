b4f8b3302d5d91e7ee36139ca00498f8a7577b11
andrpet <andrpet@gmail.com>
2016-02-01 17:42:19 +0100

    public room internationalization

--- install.php
+++ install.php
@@ -3,8 +3,7 @@
 $path = OW::getPluginManager()->getPlugin('ode')->getRootDir() . 'langs.zip';
 BOL_LanguageService::getInstance()->importPrefixFromZip($path, 'ode');
 
-$sql = 'DROP TABLE IF EXISTS `' . OW_DB_PREFIX . 'ode_datalet`;
-CREATE TABLE `' . OW_DB_PREFIX . 'ode_datalet` (
+$sql = 'CREATE TABLE IF NOT EXISTS `' . OW_DB_PREFIX . 'ode_datalet` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ownerId` int(11) NOT NULL,
   `post` text,
@@ -20,8 +19,7 @@ CREATE TABLE `' . OW_DB_PREFIX . 'ode_datalet` (
   KEY `privacy` (`privacy`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
-DROP TABLE IF EXISTS `' . OW_DB_PREFIX . 'ode_datalet_post`;
-CREATE TABLE `' . OW_DB_PREFIX . 'ode_datalet_post` (
+CREATE TABLE IF NOT EXISTS `' . OW_DB_PREFIX . 'ode_datalet_post` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `postId` int(11) NOT NULL,
   `dataletId` int(11) NOT NULL,
@@ -32,8 +30,7 @@ CREATE TABLE `' . OW_DB_PREFIX . 'ode_datalet_post` (
   KEY `plugin` (`plugin`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
-DROP TABLE IF EXISTS `' . OW_DB_PREFIX . 'ode_settings`;
-CREATE TABLE `' . OW_DB_PREFIX . 'ode_settings` (
+CREATE TABLE IF NOT EXISTS `' . OW_DB_PREFIX . 'ode_settings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `key` TEXT,
   `value` MEDIUMTEXT,
