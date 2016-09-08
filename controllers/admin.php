ee686549ca403850b5e6f8c3c0bdb10a3c249664
palmieri@dia.unisa.it <palmieri@dia.unisa.it>
2016-09-08 13:21:07 +0100

    helper updated QUICK

--- controllers/admin.php
+++ controllers/admin.php
@@ -20,7 +20,7 @@ class ODE_CTRL_Admin extends ADMIN_CTRL_Abstract
         /* DEEP ULR */
         $deepUrl = new TextField('deep_url');
         $preference = BOL_PreferenceService::getInstance()->findPreference('ode_deep_url');
-        $ode_deep_url = empty($preference) ? "http://deep.routetopa.eu/DEEP/" : $preference->defaultValue;
+        $ode_deep_url = empty($preference) ? "http://192.168.37.145/DEEalerProvider/DEEP/" : $preference->defaultValue;
         $deepUrl->setValue($ode_deep_url);
         $deepUrl->setRequired();
         $form->addElement($deepUrl);
@@ -28,7 +28,7 @@ class ODE_CTRL_Admin extends ADMIN_CTRL_Abstract
         /* DEEP DATALET LIST */
         $deepDataletList = new TextField('deep_datalet_list');
         $preference = BOL_PreferenceService::getInstance()->findPreference('ode_deep_datalet_list');
-        $ode_deep_datalet_list = empty($preference) ? "http://deep.routetopa.eu/DEEP/datalets-list" :  $preference->defaultValue;
+        $ode_deep_datalet_list = empty($preference) ? "http://192.168.37.145/DEEalerProvider/DEEP/datalets-list" :  $preference->defaultValue;
         $deepDataletList->setValue($ode_deep_datalet_list);
         $deepDataletList->setRequired();
         $form->addElement($deepDataletList);
@@ -36,7 +36,7 @@ class ODE_CTRL_Admin extends ADMIN_CTRL_Abstract
         /* DEEP CLIENT */
         $deepClient = new TextField('deep_client');
         $preference = BOL_PreferenceService::getInstance()->findPreference('ode_deep_client');
-        $ode_deep_client = empty($preference) ? "http://deep.routetopa.eu/DEEPCLIENT/js/deepClient.js" : $preference->defaultValue;
+        $ode_deep_client = empty($preference) ? "http://192.168.37.145/DEEalerProvider/DEEPCLIENT/js/deepClient.js" : $preference->defaultValue;
         $deepClient->setValue($ode_deep_client);
         $deepClient->setRequired();
         $form->addElement($deepClient);
@@ -44,7 +44,7 @@ class ODE_CTRL_Admin extends ADMIN_CTRL_Abstract
         /* WEBCOMPONENT JS */
         $webcomponents = new TextField('webcomponents_js');
         $preference = BOL_PreferenceService::getInstance()->findPreference('ode_webcomponents_js');
-        $ode_webcomponents_js = empty($preference) ? "http://deep.routetopa.eu/COMPONENTS/bower_components/webcomponentsjs/webcomponents-lite.js" : $preference->defaultValue;
+        $ode_webcomponents_js = empty($preference) ? "http://192.168.37.145/DEEalerProvider/COMPONENTS/bower_components/webcomponentsjs/webcomponents-lite.js" : $preference->defaultValue;
         $webcomponents->setValue($ode_webcomponents_js);
         $webcomponents->setRequired();
         $form->addElement($webcomponents);
