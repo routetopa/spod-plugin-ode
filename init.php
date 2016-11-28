9c7e9d104776dc8a6e040442a190c319be941c03
andrpet <andrpet@gmail.com>
2016-11-28 17:15:09 +0100

    added new datalet/controllet to DEEP configuration XML

--- init.php
+++ init.php
@@ -20,6 +20,11 @@ $preference = BOL_PreferenceService::getInstance()->findPreference('ode_webcompo
 $ode_webcomponents_js = empty($preference) ? "" : $preference->defaultValue;
 define("ODE_WEBCOMPONENTS_JS", $ode_webcomponents_js);
 
+$preference = BOL_PreferenceService::getInstance()->findPreference('spodpr_components_url');
+$spodpr_components_url = empty($preference) ? "" : $preference->defaultValue;
+define("SPODPR_COMPONENTS_URL", $spodpr_components_url);
+
+
 OW::getRouter()->addRoute(new OW_Route('ode-settings', '/ode/settings', 'ODE_CTRL_Admin', 'settings'));
 OW::getRouter()->addRoute(new OW_Route('ode-providers', '/ode/providers', 'ODE_CTRL_Admin', 'providers'));
 
