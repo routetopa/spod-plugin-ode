0bbd2b007dc7e7827d750297168b67beedd21af1
andrpet <andrpet@gmail.com>
2017-06-30 11:12:36 +0200

    update

--- static/js/ode.js
+++ static/js/ode.js
@@ -125,6 +125,9 @@ ODE.savedDataletListener = function(e)
         case 'agora' :
             AGORA.dataltet_preview_added();
             ODE.loadDatalet(data.datalet, data.params, data.fields, data.staticData.replace(new RegExp("'", 'g'), " "), ODE.commentTarget);
+        case 'discussion' :
+            SPODDISCUSSION.dataltet_preview_added();
+            ODE.loadDatalet(data.datalet, data.params, data.fields, data.staticData.replace(new RegExp("'", 'g'), " "), ODE.commentTarget);
             break;
         case 'event' :
         case 'forum' :
