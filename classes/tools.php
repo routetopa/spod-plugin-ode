bd25a2575550f39e6cd0c3ddfc986eaebd29e3fa
andrpet <andrpet@gmail.com>
2017-10-04 13:12:01 +0200

    update

--- classes/tools.php
+++ classes/tools.php
@@ -35,4 +35,57 @@ class ODE_CLASS_Tools
         return $definitions;
     }
 
+    public function decodeJwt($jwt)
+    {
+        //$header = $authorization_header;
+        //$jwt_access_token = substr( $header, 7 ); // Remove "Bearer " string
+        $jwt_access_token = $jwt;
+
+        $separator = '.';
+
+        if (2 !== substr_count($jwt_access_token, $separator)) {
+            throw new \Exception("Incorrect access token format");
+        }
+
+        list($header, $payload, $signature) = explode($separator, $jwt_access_token);
+
+        $decoded_signature = base64_decode(str_replace(array('-', '_'), array('+', '/'), $signature));
+
+        // The header and payload are signed together
+        $payload_to_verify = utf8_decode($header . $separator . $payload);
+
+        // however you want to load your public key
+        $public_key = file_get_contents( OW::getPluginManager()->getPlugin('ode')->getRootDir() . '/cert/pubkey.pem' );
+
+        // default is SHA256
+        $verified = openssl_verify($payload_to_verify, $decoded_signature, $public_key, OPENSSL_ALGO_SHA256);
+
+        if ($verified !== 1) {
+            throw new Exception("Cannot verify signature");
+        }
+
+        return base64_decode($payload);
+    }
+
+    public function getUserFromJWT($jwt)
+    {
+        try {
+            if(empty($jwt))
+                throw new Exception("No jwt provided");
+
+            $decoded_token = $this->decodeJwt($jwt);
+            if (!empty($decoded_token)) {
+                $decoded_token = json_decode($decoded_token);
+                $user = BOL_UserService::getInstance()->findByEmail($decoded_token->email);
+                if ($user)
+                    return $user->id;
+            }
+        }catch (Exception $e)
+        {
+            throw new Exception($e->getMessage());
+        }
+
+        throw new Exception("User " . $decoded_token->email . " not recognized");
+    }
+
 }
