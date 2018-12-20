89b8e9bdbb511504d77ad18bb133a9af58d0fc08
andrpet <andrpet@gmail.com>
2018-12-20 12:19:52 +0100

    update

--- components/preview.php
+++ components/preview.php
@@ -5,6 +5,7 @@ class ODE_CMP_Preview extends OW_Component
     public function __construct($component="data-sevc-controllet")
     {
         $this->assign("component", $component);
+        $this->assign('urlHome', OW_URL_HOME );
 
         $js = UTIL_JsGenerator::composeJsString('
                 ODE.ode_dataset_list = {$ode_dataset_list}
